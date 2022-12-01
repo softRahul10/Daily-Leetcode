a0c95a6523ff4a2401e2e0dbaa37ec714d89df5c
softRahul10 <softrahul10@gmail.com>
2022-12-01 22:34:36 +0530

    Progress Bar Feature Added

---
+++ progress.js
@@ -0,0 +1,16 @@
+function updateProgress() {
+    const ele = document.querySelector(".bar");
+    const numberOfQuestions = document.querySelector(".leetcode-questions").childElementCount;
+    const questionUI = document.querySelector("#numberOfSolvedQuestions");
+    const targetUI = document.querySelector("#targetQuestion");
+
+    
+    // Calculate Percentage
+    const TARGET = 200;
+    const calculateProgress =  (numberOfQuestions*100)/TARGET;
+    questionUI.innerHTML = numberOfQuestions;
+    targetUI.innerHTML = TARGET;
+    ele.style.width = `${calculateProgress}%`;
+}
+
+updateProgress();
